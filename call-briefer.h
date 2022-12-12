acd5c418f7fd9d9aed70c36f0179db5060c53e7f
bostonsinaga <aganisautiluramnotsob@gmail.com>
2022-12-12 08:08:37 +0700

    add 'call-briefer.h' for readabilty of 'main.cpp'

---
+++ call-briefer.h
@@ -0,0 +1,160 @@
+#ifndef __CALL_BRIEFER_H__
+#define __CALL_BRIEFER_H__
+
+namespace call_briefer {
+
+    std::string checkOverwrite(
+        Menu &menu,
+        int selectedFlag,
+        int overwriteFlag,
+        std::string &fileDir_in,
+        std::string &fileDir_out
+    ) {
+        if (selectedFlag == overwriteFlag) {
+            if (menu.setAlert(
+                std::string("KML-TOWN-> No '--out [FILE_NAME]'. Are you sure to overwrite the '")
+                + fileDir_in + std::string("'?\n")
+            )) {
+                return fileDir_in;
+            }
+            else return ""; // command canceled
+        }
+        return fileDir_out;
+    }
+
+    xml::Node *cropPinsModule(
+        xml::Node *kmlNode,
+        std::vector<std::string*> axisStrVec
+    ) {
+        xml::Node *retContainerNode = nullptr;
+        
+        if (kmlNode) {
+            xml::Node *mainFolderNode = kml::searchMainFolder(kmlNode);
+
+            if (mainFolderNode) {
+                mini_tool::completeDegreeCoordinateSecondsSign(axisStrVec.at(0));
+                mini_tool::completeDegreeCoordinateSecondsSign(axisStrVec.at(1));
+
+                // repair anomaly degree signs //
+
+                for (auto &str : axisStrVec) {
+                    int ctr = 0;
+                    bool isAnomaly = false;
+
+                    std::vector<int>
+                        normalDegreeSignComplementIndexes,
+                        normalDegreeSignComplementCharCodes;
+
+                    for (auto &ch : *str) {
+                        
+                        if (ch == char(ANM_DEG_INT_CHAR_PT1)) {
+                            ch = char(DEG_INT_CHAR_PT1);
+                            isAnomaly = true;
+                        }
+                        else if (ch == char(ANM_DEG_INT_CHAR_PT2)) {
+                            ch = char(DEG_INT_CHAR_PT2);
+                            isAnomaly = true;
+                        }
+
+                        if (!isAnomaly) {
+                            if (ch == char(DEG_INT_CHAR_PT1) && ctr != str->size() - 1) {
+                                if (str->at(ctr + 1) != char(DEG_INT_CHAR_PT2)) {
+                                    normalDegreeSignComplementIndexes.push_back(ctr + 1);
+                                    normalDegreeSignComplementCharCodes.push_back(DEG_INT_CHAR_PT2);
+                                }
+                            }
+                            else if (ch == char(DEG_INT_CHAR_PT2) && ctr != 0) {
+                                if (str->at(ctr - 1) != char(DEG_INT_CHAR_PT1)) {
+                                    normalDegreeSignComplementIndexes.push_back(ctr);
+                                    normalDegreeSignComplementCharCodes.push_back(DEG_INT_CHAR_PT1);
+                                }
+                            }
+                        }
+
+                        ctr++;
+                    }
+
+                    if (!isAnomaly) {
+                        int strIndexExpandRate = 0;
+                        ctr = 0;
+
+                        for (auto &index : normalDegreeSignComplementIndexes) {
+                            str->insert(
+                                str->begin() + index + strIndexExpandRate,
+                                char(normalDegreeSignComplementCharCodes.at(ctr))
+                            );
+                            ctr++;
+                            strIndexExpandRate++;
+                        }
+                    }
+                }
+
+                // cut the pins
+                kml::Cropper kmlCropper;
+                kmlCropper.cutPins(
+                    mainFolderNode,
+                    kml::Point(*axisStrVec.at(0)),
+                    kml::Point(*axisStrVec.at(1))
+                );
+
+                // test the cropped folder
+                xml::Node *croppedFolderNode = mainFolderNode->getFirstChildByName("Folder", false);
+                
+                if (croppedFolderNode) {
+                    if (croppedFolderNode->getFirstChildByName("name", false)->getInnerText()
+                        == CROP_COMMAND_WORKING_FOLDER
+                    ) {
+                        retContainerNode = croppedFolderNode;
+                    }
+                    else std::cerr << "\n**FAILED**\n";
+                }
+                else std::cerr << "\n**FAILED**\n";
+            }
+            else std::cerr << "\n**FAILED**\n";
+        }
+        else std::cerr << "\n**FAILED**\n";
+
+        return retContainerNode;
+    }
+
+    std::vector<std::string> sortPinsModule(
+        Menu &menu,
+        xml::Node *kmlNode,
+        std::vector<std::string*> axisStrVec_in,
+        bool isReturnCoordinates
+    ) {
+        xml::Node *croppedFolderNode = cropPinsModule(
+            kmlNode,
+            axisStrVec_in
+        );
+
+        // pins sorting //
+        kml::Sorter sorter;
+        std::vector<std::string> axisStrVec_sorted = sorter.orderPins(
+            croppedFolderNode,
+            kml::Point(*axisStrVec_in.at(0)), // start point
+            isReturnCoordinates
+        );
+
+        return axisStrVec_sorted;
+    }
+
+    // write file (end of process)
+    void writeFileModule(
+        xml::Node *kmlNode,
+        std::string &fileDir_out
+    ) {
+        if (kmlNode) {
+            // set name of 'main folder' upto 'root' element as 'fileDir_out' name
+            kml::setKMLDocumentName(kmlNode, fileDir_out);
+
+            xml::Writer xmlWriter;
+            xmlWriter.stringify(fileDir_out, kmlNode);
+            std::cout << "\n**SUCCEEDED**\n";
+
+            delete kmlNode;
+        }
+    }
+}
+
+#endif // __CALL_BRIEFER_H__
--- main.cpp
+++ main.cpp
@@ -27,35 +27,7 @@
 #include "tools/kml/kml.cpp"
 #include "tools/txt/txt.h"
 #include "tools/csv/csv.h"
-
-std::string checkOverwrite(
-    Menu &menu,
-    int selectedFlag,
-    int overwriteFlag,
-    std::string &fileDir_in,
-    std::string &fileDir_out
-);
-
-xml::Node *cropPinsModule(
-    xml::Node *kmlNode,
-    std::vector<std::string*> axisStrVec
-);
-
-std::vector<std::string> sortPinsModule(
-    Menu &menu,
-    xml::Node *kmlNode,
-    std::vector<std::string*> axisStrVec_in,
-    bool isReturnCoordinates
-);
-
-void writeFileModule(
-    xml::Node *kmlNode,
-    std::string &fileDir_out
-);
-
-//////////
-// MAIN //
-//////////
+#include "call-briefer.h"
 
 int main(int argc, char *argv[]) {
 
@@ -145,7 +117,7 @@ int main(int argc, char *argv[]) {
     ) {
         kml::Cropper(PRINT_NOTIFICATION, &menu);
 
-        std::string fileDir_check = checkOverwrite(
+        std::string fileDir_check = call_briefer::checkOverwrite(
             menu,
             SELECTED_FLAG,
             KML_CROP_OVERWRITE_FLAG,
@@ -157,11 +129,11 @@ int main(int argc, char *argv[]) {
             xml::Reader kmlReader;
             xml::Node *kmlNode = kmlReader.fileParse(inputStrings.at(2));
 
-            cropPinsModule(
+            call_briefer::cropPins(
                 kmlNode,
                 {&inputStrings.at(4), &inputStrings.at(6)}
             );
-            writeFileModule(kmlNode, fileDir_check);
+            call_briefer::writeFile(kmlNode, fileDir_check);
         }
     }
     else if (
@@ -171,7 +143,7 @@ int main(int argc, char *argv[]) {
         kml::Cropper(PRINT_NOTIFICATION, &menu);
         kml::Sorter(PRINT_NOTIFICATION, &menu);
 
-        std::string fileDir_check = checkOverwrite(
+        std::string fileDir_check = call_briefer::checkOverwrite(
             menu,
             SELECTED_FLAG,
             KML_SORT_OVERWRITE_FLAG,
@@ -183,13 +155,13 @@ int main(int argc, char *argv[]) {
             xml::Reader kmlReader;
             xml::Node *kmlNode = kmlReader.fileParse(inputStrings.at(2));
 
-            sortPinsModule(
+            call_briefer::sortPins(
                 menu,
                 kmlNode,
                 {&inputStrings.at(4), &inputStrings.at(6)},
                 false
             );
-            writeFileModule(kmlNode, fileDir_check);
+            call_briefer::writeFile(kmlNode, fileDir_check);
         }
     }
     else if (
@@ -200,157 +172,4 @@ int main(int argc, char *argv[]) {
     }
     
     return 0;
-}
-
-std::string checkOverwrite(
-    Menu &menu,
-    int selectedFlag,
-    int overwriteFlag,
-    std::string &fileDir_in,
-    std::string &fileDir_out
-) {
-    if (selectedFlag == overwriteFlag) {
-        if (menu.setAlert(
-            std::string("KML-TOWN-> No '--out [FILE_NAME]'. Are you sure to overwrite the '")
-            + fileDir_in + std::string("'?\n")
-        )) {
-            return fileDir_in;
-        }
-        else return ""; // command canceled
-    }
-    return fileDir_out;
-}
-
-xml::Node *cropPinsModule(
-    xml::Node *kmlNode,
-    std::vector<std::string*> axisStrVec
-) {
-    xml::Node *retContainerNode = nullptr;
-    
-    if (kmlNode) {
-        xml::Node *mainFolderNode = kml::searchMainFolder(kmlNode);
-
-        if (mainFolderNode) {
-            mini_tool::completeDegreeCoordinateSecondsSign(axisStrVec.at(0));
-            mini_tool::completeDegreeCoordinateSecondsSign(axisStrVec.at(1));
-
-            // repair anomaly degree signs //
-
-            for (auto &str : axisStrVec) {
-                int ctr = 0;
-                bool isAnomaly = false;
-
-                std::vector<int>
-                    normalDegreeSignComplementIndexes,
-                    normalDegreeSignComplementCharCodes;
-
-                for (auto &ch : *str) {
-                    
-                    if (ch == char(ANM_DEG_INT_CHAR_PT1)) {
-                        ch = char(DEG_INT_CHAR_PT1);
-                        isAnomaly = true;
-                    }
-                    else if (ch == char(ANM_DEG_INT_CHAR_PT2)) {
-                        ch = char(DEG_INT_CHAR_PT2);
-                        isAnomaly = true;
-                    }
-
-                    if (!isAnomaly) {
-                        if (ch == char(DEG_INT_CHAR_PT1) && ctr != str->size() - 1) {
-                            if (str->at(ctr + 1) != char(DEG_INT_CHAR_PT2)) {
-                                normalDegreeSignComplementIndexes.push_back(ctr + 1);
-                                normalDegreeSignComplementCharCodes.push_back(DEG_INT_CHAR_PT2);
-                            }
-                        }
-                        else if (ch == char(DEG_INT_CHAR_PT2) && ctr != 0) {
-                            if (str->at(ctr - 1) != char(DEG_INT_CHAR_PT1)) {
-                                normalDegreeSignComplementIndexes.push_back(ctr);
-                                normalDegreeSignComplementCharCodes.push_back(DEG_INT_CHAR_PT1);
-                            }
-                        }
-                    }
-
-                    ctr++;
-                }
-
-                if (!isAnomaly) {
-                    int strIndexExpandRate = 0;
-                    ctr = 0;
-
-                    for (auto &index : normalDegreeSignComplementIndexes) {
-                        str->insert(
-                            str->begin() + index + strIndexExpandRate,
-                            char(normalDegreeSignComplementCharCodes.at(ctr))
-                        );
-                        ctr++;
-                        strIndexExpandRate++;
-                    }
-                }
-            }
-
-            // cut the pins
-            kml::Cropper kmlCropper;
-            kmlCropper.cutPins(
-                mainFolderNode,
-                kml::Point(*axisStrVec.at(0)),
-                kml::Point(*axisStrVec.at(1))
-            );
-
-            // test the cropped folder
-            xml::Node *croppedFolderNode = mainFolderNode->getFirstChildByName("Folder", false);
-            
-            if (croppedFolderNode) {
-                if (croppedFolderNode->getFirstChildByName("name", false)->getInnerText()
-                    == CROP_COMMAND_WORKING_FOLDER
-                ) {
-                    retContainerNode = croppedFolderNode;
-                }
-                else std::cerr << "\n**FAILED**\n";
-            }
-            else std::cerr << "\n**FAILED**\n";
-        }
-        else std::cerr << "\n**FAILED**\n";
-    }
-    else std::cerr << "\n**FAILED**\n";
-
-    return retContainerNode;
-}
-
-std::vector<std::string> sortPinsModule(
-    Menu &menu,
-    xml::Node *kmlNode,
-    std::vector<std::string*> axisStrVec_in,
-    bool isReturnCoordinates
-) {
-    xml::Node *croppedFolderNode = cropPinsModule(
-        kmlNode,
-        axisStrVec_in
-    );
-
-    // pins sorting //
-    kml::Sorter sorter;
-    std::vector<std::string> axisStrVec_sorted = sorter.orderPins(
-        croppedFolderNode,
-        kml::Point(*axisStrVec_in.at(0)), // start point
-        isReturnCoordinates
-    );
-
-    return axisStrVec_sorted;
-}
-
-// write file (end of process)
-void writeFileModule(
-    xml::Node *kmlNode,
-    std::string &fileDir_out
-) {
-    if (kmlNode) {
-        // set name of 'main folder' upto 'root' element as 'fileDir_out' name
-        kml::setKMLDocumentName(kmlNode, fileDir_out);
-
-        xml::Writer xmlWriter;
-        xmlWriter.stringify(fileDir_out, kmlNode);
-        std::cout << "\n**SUCCEEDED**\n";
-
-        delete kmlNode;
-    }
 }
