d9c721e648fc5bfea435b76168f1b3956b03b503
bostonsinaga <bostonsinga@gmail.com>
2023-05-06 17:43:15 +0700

    crop and sort for pins and paths

--- call-briefer.cpp
+++ call-briefer.cpp
@@ -3,238 +3,322 @@
 
 #include "call-briefer.h"
 
-namespace call_briefer {
+std::string call_briefer::checkOverwrite(
+    Menu &menu,
+    std::string &fileDir_in,
+    std::string &fileDir_out
+) {
+    if (fileDir_in == fileDir_out || fileDir_out == "") {
 
-    std::string checkOverwrite(
-        Menu &menu,
-        std::string &fileDir_in,
-        std::string &fileDir_out
-    ) {
-        if (fileDir_in == fileDir_out || fileDir_out == "") {
-            if (menu.setAlert(
-                std::string("KML-TOWN-> No '--out [FILE_NAME]'. Are you sure to overwrite the '")
-                + fileDir_in + std::string("'?\n")
-            )) {
-                return fileDir_in;
-            }
-            else return ""; // command canceled
+        if (menu.setAlert(
+            std::string("KML-TOWN-> No '--out [FILE_NAME]'. Are you sure to overwrite the '")
+            + fileDir_in + std::string("'?\n")
+        )) {
+            return fileDir_in;
         }
-        return fileDir_out;
+        else return ""; // command canceled
     }
 
-    std::vector<xml::Node*> cropPlacemarkFunc(
-        xml::Node *kmlNode,
-        std::vector<std::string> &axisStrVec,
-        bool isFolderInsertion, // 'false' return nodes, 'true' return empty
-        bool isIncludeFolders,  // if true the 'isFolderInsertion' must be true
-        std::string &type_paramStr
-    ) {
-        xml::Node *retContainerNode = nullptr;
-        std::vector<xml::Node*> croppedPlacemarkNodes;
-        bool isSucceeded = true;
-        
-        if (kmlNode) {
-            xml::Node *mainFolderNode = kml::General().searchMainFolder(kmlNode);
+    return fileDir_out;
+}
 
-            if (mainFolderNode) {
-                mini_tool::completeDegreeCoordinateSecondsSign(axisStrVec.at(0));
-                mini_tool::completeDegreeCoordinateSecondsSign(axisStrVec.at(1));
+std::vector<xml::Node*> call_briefer::cropPlacemarkFunc(
+    xml::Node *kmlNode,
+    std::vector<std::string> &axisStrVec,
+    std::string type_paramStr,
+    bool isFolderInsertion,  // 'false' return nodes, 'true' return empty
+    bool isIncludeFolders    // if true the 'isFolderInsertion' must be true
+) {
+    xml::Node *retContainerNode = nullptr;
+    std::vector<xml::Node*> croppedPlacemarkNodes;
+    bool isSucceeded = true;
+    
+    if (kmlNode) {
+        xml::Node *mainFolderNode = kml::General().searchMainFolder(kmlNode);
 
-                // repair anomaly degree signs //
+        if (mainFolderNode) {
+            repairAnomalyDegreeSign(axisStrVec);
 
-                for (auto &str : axisStrVec) {
-                    int ctr = 0;
-                    bool isAnomaly = false;
+            /*  CROPPER
+                may return nodes if 'isFolderInsertion' false
+                (actually if there were placemarks in selection
+                otherwise return empty as on 'isFolderInsertion' true)
+            */
+            kml::Cropper kmlCropper;
 
-                    std::vector<int>
-                        normalDegreeSignComplementIndexes,
-                        normalDegreeSignComplementCharCodes;
+            int typeFlag;
+            enum {PIN_TYPE, PATH_TYPE, ALL_TYPE};
 
-                    for (auto &ch : str) {
-                        
-                        if (ch == char(ANM_DEG_INT_CHAR_PT1)) {
-                            ch = char(DEG_INT_CHAR_PT1);
-                            isAnomaly = true;
-                        }
-                        else if (ch == char(ANM_DEG_INT_CHAR_PT2)) {
-                            ch = char(DEG_INT_CHAR_PT2);
-                            isAnomaly = true;
-                        }
+            if (mini_tool::isStringContains(type_paramStr, "pin", true)) {
+                typeFlag = PIN_TYPE;
+            }
+            else if (mini_tool::isStringContains(type_paramStr, "path", true)) {
+                typeFlag = PATH_TYPE;
+            }
+            else if (mini_tool::isStringContains(type_paramStr, "all", true)) {
+                typeFlag = ALL_TYPE;
+            }
+            // prevent wrong type string
+            else {
+                std::cerr
+                    << "KML-TOWN-> Placemarks type input warning. Unknown type of '"
+                    << type_paramStr << "'. Default set to 'pin'\n";
 
-                        if (!isAnomaly) {
-                            if (ch == char(DEG_INT_CHAR_PT1) && ctr != str.size() - 1) {
-                                if (str.at(ctr + 1) != char(DEG_INT_CHAR_PT2)) {
-                                    normalDegreeSignComplementIndexes.push_back(ctr + 1);
-                                    normalDegreeSignComplementCharCodes.push_back(DEG_INT_CHAR_PT2);
-                                }
-                            }
-                            else if (ch == char(DEG_INT_CHAR_PT2) && ctr != 0) {
-                                if (str.at(ctr - 1) != char(DEG_INT_CHAR_PT1)) {
-                                    normalDegreeSignComplementIndexes.push_back(ctr);
-                                    normalDegreeSignComplementCharCodes.push_back(DEG_INT_CHAR_PT1);
-                                }
-                            }
-                        }
+                typeFlag = PIN_TYPE;
+            }
 
-                        ctr++;
-                    }
+            if (typeFlag == PIN_TYPE) {
+                croppedPlacemarkNodes = kmlCropper.cutPins(
+                    mainFolderNode,
+                    kml::Point(axisStrVec.at(0)),
+                    kml::Point(axisStrVec.at(1)),
+                    isFolderInsertion,
+                    isIncludeFolders,
+                    false
+                );
+            }
+            else if (typeFlag == PATH_TYPE) {
+                croppedPlacemarkNodes = kmlCropper.cutPaths(
+                    mainFolderNode,
+                    kml::Point(axisStrVec.at(0)),
+                    kml::Point(axisStrVec.at(1)),
+                    isFolderInsertion,
+                    isIncludeFolders,
+                    false
+                );
+            }
+            else if (typeFlag == ALL_TYPE) {
+                croppedPlacemarkNodes = kmlCropper.cutAll(
+                    mainFolderNode,
+                    kml::Point(axisStrVec.at(0)),
+                    kml::Point(axisStrVec.at(1)),
+                    isIncludeFolders
+                );
+            }
+            
+            if (isFolderInsertion) {
 
-                    if (!isAnomaly) {
-                        int strIndexExpandRate = 0;
-                        ctr = 0;
-
-                        for (auto &index : normalDegreeSignComplementIndexes) {
-                            str.insert(
-                                str.begin() + index + strIndexExpandRate,
-                                char(normalDegreeSignComplementCharCodes.at(ctr))
-                            );
-                            ctr++;
-                            strIndexExpandRate++;
-                        }
-                    }
-                }
-
-                /*  CROPPER
-                    may return nodes if 'isFolderInsertion' false
-                    (actually if there were pins in selection
-                    otherwise return empty as on 'isFolderInsertion' true)
-                */
-                kml::Cropper kmlCropper;
-
-                if (mini_tool::isStringContains(type_paramStr, "pin", true)) {
-                    croppedPlacemarkNodes = kmlCropper.cutPins(
-                        mainFolderNode,
-                        kml::Point(axisStrVec.at(0)),
-                        kml::Point(axisStrVec.at(1)),
-                        isFolderInsertion,
-                        isIncludeFolders,
-                        false
-                    );
-                }
-                else if (mini_tool::isStringContains(type_paramStr, "path", true)) {
-                    croppedPlacemarkNodes = kmlCropper.cutPaths(
-                        mainFolderNode,
-                        kml::Point(axisStrVec.at(0)),
-                        kml::Point(axisStrVec.at(1)),
-                        isFolderInsertion,
-                        isIncludeFolders,
-                        false
-                    );
-                }
-                else if (mini_tool::isStringContains(type_paramStr, "all", true)) {
-                    croppedPlacemarkNodes = kmlCropper.cutAll(
-                        mainFolderNode,
-                        kml::Point(axisStrVec.at(0)),
-                        kml::Point(axisStrVec.at(1)),
-                        isIncludeFolders
-                    );
-                }
-
-                // test the cropped folder //
-
+                // test the cropped folder
                 xml::Node *croppedFolderNode = (
                     mainFolderNode->getFirstChildByName("Folder")
                 );
                 
-                if (croppedFolderNode && isFolderInsertion) {
+                if (croppedFolderNode) {
                     if (kml::Placemark().getDataText(croppedFolderNode, "name")
                         == CROP_COMMAND_WORKING_FOLDER
                     ) {
-                        retContainerNode = croppedFolderNode; // 'croppedPlacemarkNodes' is empty
+                        retContainerNode = croppedFolderNode; // 'croppedPlacemarkNodes' is single
                     }
                     else isSucceeded = false;
                 }
+                else isSucceeded = false;
             }
-            else isSucceeded = false;
+
+            /*
+                not 'isFolderInsertion' means 'croppedPlacemarkNodes'
+                are placemarks instead a folder (dualism)
+            */
         }
         else isSucceeded = false;
+    }
+    else isSucceeded = false;
 
-        if (isSucceeded) {
-            if (croppedPlacemarkNodes.size() == 0) {
-                if (isFolderInsertion) {
-                    return std::vector<xml::Node*>{retContainerNode};
-                }
-                else { /* no pins detected on 'isFolderInsertion' false (no cropped folder) */
-                    std::cerr << "\n**FAILED**\n";
-                    return std::vector<xml::Node*>{};
-                }
-            }
-            else return croppedPlacemarkNodes;
-        }
-        else {
-            std::cerr << "\n**FAILED**\n";
-            return std::vector<xml::Node*>{};
-        }
+    if (isSucceeded) {
+        return croppedPlacemarkNodes;
+    }
+    else return std::vector<xml::Node*>{};
+}
+
+std::vector<xml::Node*> call_briefer::sortPlacemarksFunc(
+    xml::Node *kmlNode,
+    std::vector<std::string> &axisStrVec,
+    std::string type_paramStr,
+    bool isFolderInsertion,  // 'false' return nodes, 'true' return empty
+    bool isIncludeFolders    // if true the 'isFolderInsertion' must be true
+) {
+    int typeFlag;
+    enum {PIN_TYPE, PATH_TYPE, ALL_TYPE};
+
+    if (mini_tool::isStringContains(type_paramStr, "pin", true)) {
+        typeFlag = PIN_TYPE;
+    }
+    else if (mini_tool::isStringContains(type_paramStr, "path", true)) {
+        typeFlag = PATH_TYPE;
+    }
+    else if (mini_tool::isStringContains(type_paramStr, "all", true)) {
+        typeFlag = ALL_TYPE;
     }
 
-    std::vector<xml::Node*> sortPinsFunc(
-        Menu &menu,
-        xml::Node *kmlNode,
-        std::vector<std::string> &axisStrVec,
-        bool isFolderInsertion // 'false' return nodes, 'true' return empty
-    ) {
-        std::string typeStr = "pins"; // temporary
+    // separately repair 'axisStrVec.at(0)' by copy the value
+    std::string firstAxisStr = axisStrVec.at(0);
+    repairAnomalyDegreeSign(firstAxisStr);
+    kml::Point startPt = kml::Point(firstAxisStr);
 
-        /* 'dualismVector' may contains nodes for command that involves 'sorter' */
-        std::vector<xml::Node*> dualismVector = cropPlacemarkFunc(
+    std::vector<xml::Node*> sortedPlacemarks_arr[2];
+    std::vector<std::string> typeNamesVec;
+    std::vector<int> typeFlagVec;
+
+    if (typeFlag != ALL_TYPE) {
+        typeNamesVec.push_back(type_paramStr);
+        typeFlagVec.push_back(typeFlag);
+    }
+    else {
+        typeNamesVec = {"pin", "path"};
+        typeFlagVec = {PIN_TYPE, PATH_TYPE};
+    }
+
+    for (int i = 0; i < typeNamesVec.size(); i++) {
+
+        std::vector<xml::Node*> placemarks = cropPlacemarkFunc(
             kmlNode,
             axisStrVec,
-            isFolderInsertion,
+            typeNamesVec.at(i),
             false,
-            typeStr
+            false
         );
 
-        if (dualismVector.size() > 0) {
-            kml::Sorter kmlSorter;
-
-            std::vector<xml::Node*> sortedPinNodes = kmlSorter.orderPins(
-                dualismVector,
-                kml::Point(axisStrVec.at(0)), // start point
-                isFolderInsertion
+        if (placemarks.size() > 0) {
+            sortedPlacemarks_arr[i] = kml::Sorter().orderApart(
+                placemarks,
+                startPt,
+                typeFlagVec.at(i),
+                isFolderInsertion,
+                isIncludeFolders
             );
-
-            return sortedPinNodes;
         }
-        else return std::vector<xml::Node*>{};
+        else {
+            std::cerr << "KML-> Sorting error. No cropped " << typeNamesVec.at(i) << "s as input\n";
+        }
     }
 
-    // write file (end of process)
-    void writeFileFunc(
-        xml::Node *kmlNode,
-        std::string &fileDir_out
-    ) {
-        if (kmlNode) {
-            // set name of 'main folder' upto 'root' element as 'fileDir_out' name
-            kml::General().setKMLDocumentName(kmlNode, fileDir_out);
+    // returns folder single vector
+    if (isFolderInsertion && typeFlag == ALL_TYPE) {
+        return kml::Sorter().orderAll(sortedPlacemarks_arr, isIncludeFolders);
+    }
+    /*
+        returns placemarks vector if 'isFolderInsertion' true
+        otherwise returns one type folder single vector
+    */
+    else {
+        std::vector<xml::Node*> returnSortedPlacemarks;
 
-            xml::Writer xmlWriter;
-            xmlWriter.stringify(fileDir_out, kmlNode);
-            std::cout << "\n**SUCCEEDED**\n";
-
-            delete kmlNode;
+        for (int i = 0; i < typeFlagVec.size(); i++) {
+            returnSortedPlacemarks.insert(
+                returnSortedPlacemarks.end(),
+                sortedPlacemarks_arr[i].begin(),
+                sortedPlacemarks_arr[i].end()
+            );
         }
-        else std::cerr << "\n**FAILED**\n";
+
+        return returnSortedPlacemarks;
     }
 
-    // return command working folder
-    xml::Node *selectFunctionByType(
-        std::string inputType,
-        const std::vector<std::string> &seqTypeStrVec,
-        const std::vector<std::function<xml::Node*()>> &seqFuncVec
-    ) {
-        // 'seqTypeStrVec' and 'seqFuncVec' must equal in size
-        for (int i = 0; i < seqTypeStrVec.size(); i++) {
-            if (mini_tool::isStringContains(inputType, seqTypeStrVec.at(i), true)) {
-                return seqFuncVec.at(i)();
+    return std::vector<xml::Node*>{};
+}
+
+// write file (end of process)
+void call_briefer::writeFileFunc(
+    xml::Node *kmlNode,
+    std::string &fileDir_out
+) {
+    if (kmlNode) {
+        // set name of 'main folder' upto 'root' element as 'fileDir_out' name
+        kml::General().setKMLDocumentName(kmlNode, fileDir_out);
+
+        xml::Writer xmlWriter;
+        xmlWriter.stringify(fileDir_out, kmlNode);
+        std::cout << "\n**SUCCEEDED**\n";
+
+        delete kmlNode;
+    }
+    else std::cerr << "\n**FAILED**\n";
+}
+
+// return command working folder
+xml::Node *call_briefer::selectFunctionByType(
+    std::string inputType,
+    const std::vector<std::string> &seqTypeStrVec,
+    const std::vector<std::function<xml::Node*()>> &seqFuncVec
+) {
+    // 'seqTypeStrVec' and 'seqFuncVec' must equal in size
+    for (int i = 0; i < seqTypeStrVec.size(); i++) {
+        if (mini_tool::isStringContains(inputType, seqTypeStrVec.at(i), true)) {
+            return seqFuncVec.at(i)();
+        }
+    }
+
+    // set default call to first function
+    std::cerr
+        << "KML-TOWN-> Placemarks type input warning. Unknown type of '"
+        << inputType << "'. Default set to '" << seqTypeStrVec.at(0) << "'\n";
+    return seqFuncVec.at(0)();
+}
+
+void call_briefer::repairAnomalyDegreeSign(std::vector<std::string> &axisStrVec) {
+
+    for (int i = 0; i < axisStrVec.size(); i++) {
+        mini_tool::completeDegreeCoordinateSecondsSign(axisStrVec.at(i));
+    }
+
+    // repair anomaly degree signs //
+
+    for (auto &str : axisStrVec) {
+        int ctr = 0;
+        bool isAnomaly = false;
+
+        std::vector<int>
+            normalDegreeSignComplementIndexes,
+            normalDegreeSignComplementCharCodes;
+
+        for (auto &ch : str) {
+            
+            if (ch == char(ANM_DEG_INT_CHAR_PT1)) {
+                ch = char(DEG_INT_CHAR_PT1);
+                isAnomaly = true;
+            }
+            else if (ch == char(ANM_DEG_INT_CHAR_PT2)) {
+                ch = char(DEG_INT_CHAR_PT2);
+                isAnomaly = true;
+            }
+
+            if (!isAnomaly) {
+                if (ch == char(DEG_INT_CHAR_PT1) && ctr != str.size() - 1) {
+                    if (str.at(ctr + 1) != char(DEG_INT_CHAR_PT2)) {
+                        normalDegreeSignComplementIndexes.push_back(ctr + 1);
+                        normalDegreeSignComplementCharCodes.push_back(DEG_INT_CHAR_PT2);
+                    }
+                }
+                else if (ch == char(DEG_INT_CHAR_PT2) && ctr != 0) {
+                    if (str.at(ctr - 1) != char(DEG_INT_CHAR_PT1)) {
+                        normalDegreeSignComplementIndexes.push_back(ctr);
+                        normalDegreeSignComplementCharCodes.push_back(DEG_INT_CHAR_PT1);
+                    }
+                }
+            }
+
+            ctr++;
+        }
+
+        if (!isAnomaly) {
+            int strIndexExpandRate = 0;
+            ctr = 0;
+
+            for (auto &index : normalDegreeSignComplementIndexes) {
+                str.insert(
+                    str.begin() + index + strIndexExpandRate,
+                    char(normalDegreeSignComplementCharCodes.at(ctr))
+                );
+                ctr++;
+                strIndexExpandRate++;
             }
         }
-
-        // set default call to first function
-        std::cerr
-            << "KML-TOWN-> Placemarks type input warning. Unknown type of '"
-            << inputType << "'. Default set to '" << seqTypeStrVec.at(0) << "'\n";
-        return seqFuncVec.at(0)();
     }
+}
+
+void call_briefer::repairAnomalyDegreeSign(std::string &axisStr) {
+    std::vector<std::string> axisStrVec = {axisStr};
+    repairAnomalyDegreeSign(axisStrVec);
+    axisStr = axisStrVec.front();
 }
 
 #endif // __CALL_BRIEFER_CPP__
--- call-briefer.h
+++ call-briefer.h
@@ -1,41 +1,46 @@
 #ifndef __CALL_BRIEFER_H__
 #define __CALL_BRIEFER_H__
 
-namespace call_briefer {
+class call_briefer {
+    public:
+        static std::string checkOverwrite(
+            Menu &menu,
+            std::string &fileDir_in,
+            std::string &fileDir_out
+        );
 
-    std::string checkOverwrite(
-        Menu &menu,
-        std::string &fileDir_in,
-        std::string &fileDir_out
-    );
+        static std::vector<xml::Node*> cropPlacemarkFunc(
+            xml::Node *kmlNode,
+            std::vector<std::string> &axisStrVec,
+            std::string type_paramStr,
+            bool isFolderInsertion,  // 'false' return nodes, 'true' return empty
+            bool isIncludeFolders    // if true the 'isFolderInsertion' must be true
+        );
 
-    std::vector<xml::Node*> cropPlacemarkFunc(
-        xml::Node *kmlNode,
-        std::vector<std::string> &axisStrVec,
-        bool isFolderInsertion, // 'false' return nodes, 'true' return empty
-        bool isIncludeFolders,  // if true the 'isFolderInsertion' must be true
-        std::string &type_paramStr
-    );
+        static std::vector<xml::Node*> sortPlacemarksFunc(
+            xml::Node *kmlNode,
+            std::vector<std::string> &axisStrVec,
+            std::string type_paramStr,
+            bool isFolderInsertion,  // 'false' return nodes, 'true' return empty
+            bool isIncludeFolders    // if true the 'isFolderInsertion' must be true
+        );
 
-    std::vector<xml::Node*> sortPinsFunc(
-        Menu &menu,
-        xml::Node *kmlNode,
-        std::vector<std::string> &axisStrVec,
-        bool isFolderInsertion // 'false' return nodes, 'true' return empty
-    );
+        // write file (end of process)
+        static void writeFileFunc(
+            xml::Node *kmlNode,
+            std::string &fileDir_out
+        );
 
-    // write file (end of process)
-    void writeFileFunc(
-        xml::Node *kmlNode,
-        std::string &fileDir_out
-    );
+        // return command working folder
+        static xml::Node *selectFunctionByType(
+            std::string inputType,
+            const std::vector<std::string> &seqTypeStrVec,
+            const std::vector<std::function<xml::Node*()>> &seqFuncVec
+        );
 
-    // return command working folder
-    xml::Node *selectFunctionByType(
-        std::string placemarksType,
-        const std::function<xml::Node*()> &funcPins,
-        const std::function<xml::Node*()> &funcPaths
-    );
-}
+    private:
+        static void repairAnomalyDegreeSign(std::vector<std::string> &axisStrVec);
+        static void repairAnomalyDegreeSign(std::string &axisStr);
+};
 
 #endif // __CALL_BRIEFER_H__
