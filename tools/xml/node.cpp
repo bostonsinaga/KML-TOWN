544ae048d96af2f3754e70a723d0dfdc387f3cd4
bostonsinaga <bostonsinga@gmail.com>
2024-08-05 12:01:40 +0700

    Set indentation to 2 spaces

--- tools/xml/node.cpp
+++ tools/xml/node.cpp
@@ -4,38 +4,37 @@
 #include "node.h"
 
 Node::Node(std::string name_in, Node *parent_in) {
-    name = name_in;
-    if (parent_in) {
-        parent = parent_in;
-        parent->addChild(this);
-    }
+  name = name_in;
+  if (parent_in) {
+    parent = parent_in;
+    parent->addChild(this);
+  }
 }
 
-/*  ATTENTION!
-    Destruction using 'delete node;' is deprecated.
-    (Can causing error)
-
-    Destruction of object expected using:
-
-    'if (parent) parent->removeChild(child, true);'
-    
-    or
-
-    'node->removeFromParent(true);'
-
-    But this not working with a root node.
-    Which have no parent.
-*/
+/**
+ * ATTENTION!
+ * Destruction using 'delete node;' is bad.
+ * Destruction of object expected using:
+ *
+ * 'if (parent) parent->removeChild(child, true);'
+ * 
+ * or
+ * 
+ * 'node->removeFromParent(true);'
+ *
+ * But this not working with a root node
+ * which have no parent.
+ */
 Node::~Node() {
-    for (auto &child : children) {
-        delete child;
-    }
+  for (auto &child : children) {
+    delete child;
+  }
 
-    children.clear();
-    attributes.clear();
-    parent = nullptr;
+  children.clear();
+  attributes.clear();
+  parent = nullptr;
 
-    /* parent still has this as member of children vector */
+  /** Parent still has this as member of children vector */
 }
 
 std::string Node::getName() {return name;}
@@ -44,279 +43,276 @@ std::string Node::getInnerText() {return innerText;}
 Node *Node::getParent() {return parent;}
 
 Node *Node::getRoot() {
-    Node *retNode = this;
+  Node *retNode = this;
 
-    while (retNode->getParent()) {
-        retNode = retNode->getParent();
-    }
+  while (retNode->getParent()) {
+    retNode = retNode->getParent();
+  }
 
-    return retNode;
+  return retNode;
 }
 
 std::vector<Node*> *Node::getChildren() {return &children;}
 std::vector<Attribute> *Node::getAttributes() {return &attributes;}
 
 Node *Node::getFirstChildByName(
-    std::string searchName,
-    bool isPrintMessage
+  std::string searchName,
+  bool isPrintMessage
 ) {    
-    for (auto &child : children) {
-        if (child->getName() == searchName) {
-            return child;
-        }
+  for (auto &child : children) {
+    if (child->getName() == searchName) {
+      return child;
     }
+  }
 
-    if (isPrintMessage) {
-        printChildNotFoundMessage(searchName);
-    }
-    
-    return nullptr;
+  if (isPrintMessage) {
+    printChildNotFoundMessage(searchName);
+  }
+  
+  return nullptr;
 }
 
 std::vector<Node*> Node::getChildrenByName(
-    std::string searchName,
-    bool isPrintMessage
+  std::string searchName,
+  bool isPrintMessage
 ) {
-    std::vector<Node*> retNodes;
+  std::vector<Node*> retNodes;
 
-    for (auto &child : children) {
-        if (child->getName() == searchName) {
-            retNodes.push_back(child);
-        }
+  for (auto &child : children) {
+    if (child->getName() == searchName) {
+      retNodes.push_back(child);
     }
+  }
 
-    if (retNodes.size() == 0 && isPrintMessage) {
-        printChildNotFoundMessage(searchName);
-    }
+  if (retNodes.size() == 0 && isPrintMessage) {
+    printChildNotFoundMessage(searchName);
+  }
 
-    return retNodes;
+  return retNodes;
 }
 
-// pull by name as first child from upper to lower generations
+/** Pull by name as first child from upper to lower generations */
 Node *Node::getFirstDescendantByName(
-    std::string searchName,
-    bool isPrintMessage
+  std::string searchName,
+  bool isPrintMessage
 ) {
-    for (auto &child : children) {
-        if (child->getName() == searchName) {
-            return child;
-        }
-        else {
-            Node *retNode = child->getFirstDescendantByName(searchName);
-            if (retNode) return retNode;
-        }
+  for (auto &child : children) {
+    if (child->getName() == searchName) {
+      return child;
     }
-    
-    if (isPrintMessage) {
-        printChildNotFoundMessage(searchName);
+    else {
+      Node *retNode = child->getFirstDescendantByName(searchName);
+      if (retNode) return retNode;
     }
+  }
 
-    return nullptr;
+  if (isPrintMessage) {
+    printChildNotFoundMessage(searchName);
+  }
+
+  return nullptr;
 }
 
-// pull by name as children from upper to lower generations
+/** Pull by name as children from upper to lower generations */
 std::vector<Node*> Node::getDescendantsByName(
-    std::string searchName,
-    bool isPrintMessage
+  std::string searchName,
+  bool isPrintMessage
 ) {
-    std::vector<Node*> retNodes;
+  std::vector<Node*> retNodes;
 
-    for (auto &child : children) {
+  for (auto &child : children) {
 
-        if (child->getName() == searchName) {
-            retNodes.push_back(child);
-        }
-
-        std::vector<Node*> pulledNodes = child->getDescendantsByName(searchName);
-        
-        retNodes.insert(
-            retNodes.end(),
-            pulledNodes.begin(),
-            pulledNodes.end()
-        );
+    if (child->getName() == searchName) {
+      retNodes.push_back(child);
     }
 
-    if (retNodes.size() == 0 && isPrintMessage) {
-        printChildNotFoundMessage(searchName);
-    }
+    std::vector<Node*> pulledNodes = child->getDescendantsByName(searchName);
 
-    return retNodes;
+    retNodes.insert(
+      retNodes.end(),
+      pulledNodes.begin(),
+      pulledNodes.end()
+    );
+  }
+
+  if (retNodes.size() == 0 && isPrintMessage) {
+    printChildNotFoundMessage(searchName);
+  }
+
+  return retNodes;
 }
 
 int Node::getGender() {
-    return genderFlag;
+  return genderFlag;
 }
 
 void Node::setGender(int genderFlag_in) {
-    genderFlag = genderFlag_in;
+  genderFlag = genderFlag_in;
 }
 
-// NAME AND INNER TEXT //
+/** NAME AND INNER TEXT */
 
 void Node::setName(const std::string &name_in) {
-    name = name_in;
+  name = name_in;
 }
 
 void Node::setName(std::string &name_in) {
-    name = name_in;
+  name = name_in;
 }
 
 void Node::setInnerText(const std::string &text_in) {
-    innerText = text_in;
+  innerText = text_in;
 }
 
 void Node::setInnerText(std::string &text_in) {
-    innerText = text_in;
+  innerText = text_in;
 }
 
-// ATTRIBUTE //
+/** ATTRIBUTE */
 
 void Node::setAttributes(std::vector<Attribute> attributes_in) {
-    attributes = attributes_in;
+  attributes = attributes_in;
 }
 
 void Node::addAttribute(Attribute attribute_in) {
-    attributes.push_back(attribute_in);
+  attributes.push_back(attribute_in);
 }
 
 void Node::setParent(
-    Node *parent_in,
-    bool isParentAddChild,
-    bool isSwitchParent,
-    int order
+  Node *parent_in,
+  bool isParentAddChild,
+  bool isSwitchParent,
+  int order
 ) {
-    if (isSwitchParent) {
-        removeFromParent();
-    }
-    
-    parent = parent_in;
+  if (isSwitchParent) removeFromParent();
+  parent = parent_in;
 
-    if (isParentAddChild && parent) {
-        parent->addChild(this, order);
-    }
+  if (isParentAddChild && parent) {
+    parent->addChild(this, order);
+  }
 }
 
 // 'isClean' to self delete node
 void Node::removeFromParent(bool isClean) {
-    if (parent) {
-        int ctr = 0;
+  if (parent) {
+    int ctr = 0;
 
-        for (auto &child : *parent->getChildren()) {
-            if (child == this) {
+    for (auto &child : *parent->getChildren()) {
+      if (child == this) {
 
-                parent->getChildren()->erase(
-                    parent->getChildren()->begin() + ctr,
-                    parent->getChildren()->begin() + ctr + 1
-                );
+        parent->getChildren()->erase(
+          parent->getChildren()->begin() + ctr,
+          parent->getChildren()->begin() + ctr + 1
+        );
 
-                if (isClean) delete this;
-                break;
-            }
-            ctr++;
-        }
-        parent = nullptr;
+        if (isClean) delete this;
+        break;
+      }
+      ctr++;
     }
-    else if (isClean) delete this;
+    parent = nullptr;
+  }
+  else if (isClean) delete this;
 }
 
 void Node::addChild(Node *newChild, int order) {
-
-    if (order != -1) {
-        if (order > children.size()) {
-            order = children.size();
-        }
-        children.insert(children.begin() + order, newChild);
+  if (order != -1) {
+    if (order > children.size()) {
+      order = children.size();
     }
-    else children.push_back(newChild);
+    children.insert(children.begin() + order, newChild);
+  }
+  else children.push_back(newChild);
 
-    newChild->setParent(this, false);
+  newChild->setParent(this, false);
 }
 
 void Node::addChildren(std::vector<Node*> newChildren) {
-    children.insert(
-        children.end(),
-        newChildren.begin(),
-        newChildren.end()
-    );
+  children.insert(
+    children.end(),
+    newChildren.begin(),
+    newChildren.end()
+  );
 }
 
 std::vector<Node*> Node::replaceChildren(std::vector<Node*> newChildren) {
-    std::vector<Node*> unusedChildren = releaseChildren();
-    children = newChildren;
-    return unusedChildren;
+  std::vector<Node*> unusedChildren = releaseChildren();
+  children = newChildren;
+  return unusedChildren;
 }
 
 void Node::swapChildren(int aDex, int bDex) {
-    if (aDex != -1 && bDex != -1 && aDex != bDex &&
-        children.size() > aDex && children.size() > bDex 
-    ) {
-        Node *buffer = children.at(aDex);
-        children.at(aDex) = children.at(bDex);
-        children.at(bDex) = buffer;
-    }
+  if (aDex != -1 && bDex != -1 && aDex != bDex &&
+    children.size() > aDex && children.size() > bDex 
+  ) {
+    Node *buffer = children.at(aDex);
+    children.at(aDex) = children.at(bDex);
+    children.at(bDex) = buffer;
+  }
 }
 
 void Node::swapChildren(Node *childA, Node *childB) {
-    if (childA != childB) {
+  if (childA != childB) {
 
-        int swapIndexes[2] = {-1, -1}, ctr = 0;
+    int swapIndexes[2] = {-1, -1}, ctr = 0;
 
-        for (auto &child : children) {
+    for (auto &child : children) {
 
-            if (child == childA) swapIndexes[0] = ctr;
-            else if (child == childB) swapIndexes[1] = ctr;
+      if (child == childA) swapIndexes[0] = ctr;
+      else if (child == childB) swapIndexes[1] = ctr;
 
-            if (swapIndexes[0] != -1 && swapIndexes[1] != -1) {
-                break;
-            }
+      if (swapIndexes[0] != -1 && swapIndexes[1] != -1) {
+        break;
+      }
 
-            ctr++;
-        }
-
-        swapChildren(swapIndexes[0], swapIndexes[1]);
+      ctr++;
     }
+
+    swapChildren(swapIndexes[0], swapIndexes[1]);
+  }
 }
 
-/*  WARNING!
-*   -use 'isClean' if only the child has not been deleted
-*   -don't use 'isClean' from node destructor
-*/
+/**
+ * WARNING!
+ *  -use 'isClean' if only the child has not been deleted
+ *  -don't use 'isClean' from node destructor
+ */
 void Node::removeChild(Node *exChild, bool isClean) {
-    
-    for (int i = 0; i < children.size(); i++) {
-        if (children.at(i) == exChild) {
 
-            if (isClean) delete children.at(i);
+  for (int i = 0; i < children.size(); i++) {
+    if (children.at(i) == exChild) {
 
-            children.erase(children.begin() + i, children.begin() + i + 1);
-            break;
-        }
+      if (isClean) delete children.at(i);
+
+      children.erase(children.begin() + i, children.begin() + i + 1);
+      break;
     }
+  }
 }
 
 void Node::removeChildren() {
-    for (auto &child : children) {
-        delete child;
-    }
-    children.clear();
+  for (auto &child : children) {
+    delete child;
+  }
+  children.clear();
 }
 
 std::vector<Node*> Node::releaseChildren() {
-    
-    for (auto &child : children) {
-        child->setParent(nullptr, false);
-    }
 
-    std::vector<Node*> retChildren = children;
-    children.clear();
-    return retChildren;
+  for (auto &child : children) {
+    child->setParent(nullptr, false);
+  }
+
+  std::vector<Node*> retChildren = children;
+  children.clear();
+  return retChildren;
 }
 
 void Node::printChildNotFoundMessage(std::string searchName) {
-    std::cerr
-        << "XML-> Node warning. No child named '"
-        << searchName << "' in '"
-        << name << "' node\n";
+  std::cerr
+    << "XML-> Node warning. No child named '"
+    << searchName << "' in '"
+    << name << "' node\n";
 }
 
 #endif // __XML_NODE_CPP__
--- tools/xml/node.h
+++ tools/xml/node.h
@@ -2,100 +2,100 @@
 #define __XML_NODE_H__
 
 class Attribute {
-    public:
-        Attribute(std::string name_in, std::string value_in) {
-            name = name_in;
-            value = value_in;
-        }
+public:
+  Attribute(std::string name_in, std::string value_in) {
+    name = name_in;
+    value = value_in;
+  }
 
-        std::string getName() {return name;}
-        std::string getValue() {return value;}
+  std::string getName() {return name;}
+  std::string getValue() {return value;}
 
-        void setName(std::string name_in) {
-            name = name_in;
-        }
+  void setName(std::string name_in) {
+    name = name_in;
+  }
 
-        void setValue(std::string value_in) {
-            value = value_in;
-        }
+  void setValue(std::string value_in) {
+    value = value_in;
+  }
 
-    private:
-        std::string name, value;
+private:
+  std::string name, value;
 };
 
-#define MALE_XMLGEN 0 // unable to own children
-#define FEMALE_XMLGEN 1 // able to own children
+#define MALE_XMLGEN 0  // unable to own children
+#define FEMALE_XMLGEN 1  // able to own children
 
 class Node {
-    public:
-        Node(std::string name_in, Node *parent_in = nullptr);
-        ~Node();
+public:
+  Node(std::string name_in, Node *parent_in = nullptr);
+  ~Node();
 
-        std::string getName();
-        std::string getInnerText();
-        Node *getParent();
-        Node *getRoot();
-        
-        std::vector<Node*> *getChildren();
-        std::vector<Attribute> *getAttributes();
+  std::string getName();
+  std::string getInnerText();
+  Node *getParent();
+  Node *getRoot();
+  
+  std::vector<Node*> *getChildren();
+  std::vector<Attribute> *getAttributes();
 
-        Node *getFirstChildByName(
-            std::string searchName,
-            bool isPrintMessage = false
-        );
+  Node *getFirstChildByName(
+    std::string searchName,
+    bool isPrintMessage = false
+  );
 
-        std::vector<Node*> getChildrenByName(
-            std::string searchName,
-            bool isPrintMessage = false
-        );
+  std::vector<Node*> getChildrenByName(
+    std::string searchName,
+    bool isPrintMessage = false
+  );
 
-        // pull by name as first child from upper to lower generations
-        Node *getFirstDescendantByName(
-            std::string searchName,
-            bool isPrintMessage = false
-        );
+  /** Pull by name as first child from upper to lower generations */
+  Node *getFirstDescendantByName(
+    std::string searchName,
+    bool isPrintMessage = false
+  );
 
-        // pull by name as children from upper to lower generations
-        std::vector<Node*> getDescendantsByName(
-            std::string searchName,
-            bool isPrintMessage = false
-        );
+  /** Pull by name as children from upper to lower generations */
+  std::vector<Node*> getDescendantsByName(
+    std::string searchName,
+    bool isPrintMessage = false
+  );
 
-        int getGender();
-        void setGender(int genderFlag_in);
+  int getGender();
+  void setGender(int genderFlag_in);
 
-        // NAME AND INNER TEXT //
+  /** NAME AND INNER TEXT */
 
-        void setName(const std::string &name_in);
-        void setName(std::string &name_in);
+  void setName(const std::string &name_in);
+  void setName(std::string &name_in);
 
-        void setInnerText(const std::string &text_in);
-        void setInnerText(std::string &text_in);
+  void setInnerText(const std::string &text_in);
+  void setInnerText(std::string &text_in);
 
-        // ATTRIBUTE //
+  /** ATTRIBUTE */
 
-        void setAttributes(std::vector<Attribute> attributes_in);
-        void addAttribute(Attribute attribute_in);
-        void setParent(Node *parent_in, bool isParentAddChild, bool isSwitchParent = false, int order = -1);
-        void removeFromParent(bool isClean = false);
-        void addChild(Node *newChild, int order = -1);
-        void addChildren(std::vector<Node*> newChildren);
-        std::vector<Node*> replaceChildren(std::vector<Node*> newChildren);
-        void swapChildren(int aDex, int bDex);
-        void swapChildren(Node *childA, Node *childB);
-        void removeChild(Node *exChild, bool isClean = false);
-        void removeChildren();
-        std::vector<Node*> releaseChildren();
+  void setAttributes(std::vector<Attribute> attributes_in);
+  void addAttribute(Attribute attribute_in);
+  void setParent(Node *parent_in, bool isParentAddChild, bool isSwitchParent = false, int order = -1);
+  void removeFromParent(bool isClean = false);
+  void addChild(Node *newChild, int order = -1);
+  void addChildren(std::vector<Node*> newChildren);
+  std::vector<Node*> replaceChildren(std::vector<Node*> newChildren);
+  void swapChildren(int aDex, int bDex);
+  void swapChildren(Node *childA, Node *childB);
+  void removeChild(Node *exChild, bool isClean = false);
+  void removeChildren();
+  std::vector<Node*> releaseChildren();
 
-    private:
-        void printChildNotFoundMessage(std::string searchName);
+private:
+  void printChildNotFoundMessage(std::string searchName);
 
-        int genderFlag = FEMALE_XMLGEN;
-        std::string name, innerText;
-        Node *parent = nullptr;
+  int genderFlag = FEMALE_XMLGEN;
+  std::string name, innerText;
+  Node *parent = nullptr;
 
-        std::vector<Node*> children;
-        std::vector<Attribute> attributes;
+  std::vector<Node*> children;
+  std::vector<Attribute> attributes;
 };
 
 #endif // __XML_NODE_H__
